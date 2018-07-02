de2253e90b46d87e9f4da7d90daa77ee52ce517e
anukulai <anukulai0312@gmail.com>
2018-07-02 16:55:40 +0530

    React layout

---
+++ client/dummy/dummy.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class dummy extends Component {
+  render() {
+    return (
+      <div class="row">
+      <div class="col-sm-4">
+        profile
+      </div>
+
+       <div class="col-sm-2">
+        <button>login</button>
+      </div>
+      
+       <div class="col-sm-6">
+
+      </div>
+        
+      </div>
+    )
+  }
+}
